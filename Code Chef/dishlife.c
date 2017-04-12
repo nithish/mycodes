4885fa39135816f660fe12c05bbc0f4e028a3a2c
nithish <nithish@github.com>
2017-04-13 01:06:45 +0530

    AC/Partial

--- Code Chef/dishlife.c
+++ Code Chef/dishlife.c
@@ -4,7 +4,7 @@
 typedef long long int lint;
 
 void getInput();
-lint* convertToInt(char *str);
+lint* convertToInt(lint *str,lint buff);
 void getLine(lint **arr,short i);
 lint ctoint(char a);
 void processInput(lint **arr, lint islands,lint ing);
@@ -26,75 +26,63 @@ void getInput(){
   lint **arr  = NULL,j = 0;
   scanf("%lld %lld",&islands,&ing);
   temp = ing;
-  //printf("%lld\n",islands);
   arr = malloc(islands*sizeof(lint));
-  char* str = NULL;
-  lint buff = ing+2;int a = 0;
-	str = (char*)malloc(buff);
-  //printf("-->%lld %lld",islands, ing);
+  lint* str = NULL;
+  lint buff = temp;int a = 0;
   while(j < islands){
+    str = (lint*)malloc(buff*sizeof(lint));
     lint i = 0;
-    printf("$%lld\n",j);
-    while((scanf(" %c",&str[i])==1) && i < ing) {
+    while((scanf(" %lld",&str[i])==1) && i < ing) {
         if(i == 0){
-          ing = ctoint(str[i]);
+          ing = (str[i]);
         }
         i++;
 		}
-		str[i+1] = '\0';
-  	arr[j] = malloc(sizeof(buff));
-    arr[j] = convertToInt(str);
+  	arr[j] = (lint*)malloc(buff*sizeof(lint));
+    arr[j] = convertToInt(str,buff);
     j++;
   }
   processInput(arr,islands,temp);
-  //free(arr);
 }
-lint* convertToInt(char *str){
+lint* convertToInt(lint *str,lint buff){
   lint *a = NULL;
-  lint len = strlen(str);
-  a = malloc(len * sizeof(lint));
+  a = malloc(buff * sizeof(lint));
   lint i = 0;
-  while(i < len){
-    if(str[i] != ' ' && str[i] != '\n')
-      a[i] = str[i] - '0';
+  while(i < buff){
+    a[i] = str[i];
     i++;
   }
   return a;
 }
 void processInput(lint **arr,lint islands,lint ing){
-  lint *visited = NULL,count = 0, i = 0,j = 0;
-  visited = malloc(ing*sizeof(lint));
-  int k = 0;
   if(!indcheck(arr,islands,ing)){
+    lint *visited = NULL,count = 0, i = 0,j = 0;
+    visited = malloc(ing*sizeof(lint));
+    int k = 0;
+    //printf("Inseid\n" );
     while(k < ing){
       visited[k] = -1;
       k++;
     }
   while(i < islands){
-
     lint t = arr[i][0];
     lint count1 = 0;
-    //int * sub = malloc(ing*sizeof(lint));
     for(j = 1;j <= t; j++){
       if(arr[i][j] <= ing && !contains(visited,arr[i][j],ing)){
         count++;
         add(visited,arr[i][j],ing);
-        //printf("%lld\n",count1);
       }
     }
-
     if(count == ing)
       break;
     i++;
   }
-  //printf("\n%lld:%lld %lld:%lld\n",i,islands,count,ing);
   if(i+1 < islands && count == ing){
     printf("some\n");
   }else if((i+1 == islands || i == islands ) && count == ing)
     printf("all\n");
   else
     printf("sad\n");
-    //free(visited);
   }
 }
 lint ctoint(char a){
@@ -116,15 +104,14 @@ void add(lint *v,lint i,lint size){
   v[h] = i;
 }
 int indcheck(lint **arr,lint islands,lint ing){
-  lint i = 0,j = 0,count = 0,f = 0,g = 0;
+  lint i = 0,j = 0,count = 0,f = 0,g = 0,k = 0;
   while(i < islands){
     lint *v = malloc(ing * sizeof(lint));
     while(g < ing){
       v[g] = -1;
       g++;
     }
-    lint k = arr[i][0];
-    //printf("#%lld\n",v[0]);
+    k = arr[i][0];
     for(j = 1;j<=k;j++){
       if(!contains(v,arr[i][j],ing)){
         count++;
@@ -134,12 +121,14 @@ int indcheck(lint **arr,lint islands,lint ing){
 
     if(count == ing){
       f = 1;
-      printf("some\n");
+      if( i+1 == islands)
+        printf("all\n");
+      else if(i+1 < islands)
+        printf("some\n");
       break;
     }
     count = 0;
     g = 0;
-    free(v);
     i++;
   }
   return f;
